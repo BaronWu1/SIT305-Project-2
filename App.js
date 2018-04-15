2b9734fabcaeaef3a423c5ff30f8fbc324457a6d
jundapeng111 <37976509+jundapeng111@users.noreply.github.com>
2018-04-15 22:35:36 +1000

    main menu

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Button, Alert, View } from 'react-native';
 import { AppLoading } from 'expo';
 
 // Main function
 export default class App extends React.Component {
   state = {
     isReady: false,
+    text: ''
   };
 
   // Main render function
@@ -23,20 +24,50 @@ export default class App extends React.Component {
 
     // Main screen
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.mainContainer}>
+        <View style={styles.mainTitle}>
+          <Text style={{fontSize: 21}}>Welcome to MY RPG GAME!</Text>
+        </View>
+
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>New Game</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Load Game</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Exit</Text>
+          </View>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View>
+            <Text style={{color: '#4990e2', fontSize: 17, margin: 20}}>Report a bug?</Text>
+          </View>
+        </TouchableOpacity>
+
       </View>
     );
 
+  }
+
+  _onPressButton() {
+    // Alert.alert('Yo!')
   }
 
   // Holder function for demonstrating splash
   async _cacheResourcesAsync() {
     var start = new Date().getTime();
     var end = start;
-    while(end < start + 2000) {
+    while(end < start + 0) {
       end = new Date().getTime();
     }
   }
@@ -44,10 +75,38 @@ export default class App extends React.Component {
 
 // Main screen styles
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  mainTitle: {
+    height: 80,
     justifyContent: 'center',
   },
+
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+
+    /* Primary axis = column = vertical = default */
+    flexDirection: 'column',
+
+    /* primary axis */
+    justifyContent: 'center',
+
+    /* secondary axis */
+    alignItems: 'center',
+
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+
+  button: {
+    margin: 20,
+    width: 260,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  },
+
 });
