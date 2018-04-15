5cef741e357e6d3ea8b59add4efabb4f9d377a93
BaronWu1 <wyufe@deakin.edu.au>
2018-04-15 14:53:58 +1000

    splash screen

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
 
+// Main function
 export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  // Main render function
   render() {
+    // Splash screen
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+
+    // Main screen
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
@@ -10,9 +29,20 @@ export default class App extends React.Component {
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
+
+  }
+
+  // Holder function for demonstrating splash
+  async _cacheResourcesAsync() {
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + 2000) {
+      end = new Date().getTime();
+    }
   }
 }
 
+// Main screen styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
